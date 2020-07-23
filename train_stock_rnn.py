f0e7f2029399dae3ad809e1496c6eb90746ba3f8
Aaron Borger <46955176+aborger@users.noreply.github.com>
2020-07-23 09:22:31 -0600

    Add files via upload

--- train_stock_rnn.py
+++ train_stock_rnn.py
@@ -15,21 +15,24 @@ import pandas as pd
 NUMBARS = 5
 
 dataset_train = pd.read_csv('Dataset.csv', sep=r'\s*,\s*')
-training_set = dataset_train.iloc[:,0:1].values
+training_set = dataset_train.to_numpy()
 dataset_train.head()
 
 print(training_set.shape)
 print(type(training_set))
 
+# Normalize data
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range=(0,1))
 training_set_scaled = sc.fit_transform(training_set)
 
+training_set_scaled.shape
+
 X_train = []
 y_train = []
-for i in range(10, 2000):
-  X_train.append(training_set_scaled[i-10:i,0])
-  y_train.append(training_set_scaled[i,0])
+for i in range(10, 1000):
+  X_train.append(training_set_scaled[i-10:i])
+  y_train.append(training_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
 
 print(X_train.shape)
@@ -37,18 +40,15 @@ print(y_train.shape)
 print(X_train.shape[0])
 print(X_train.shape[1])
 
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-print(X_train.shape)
-
 """Build Neural Network"""
 
 import keras
 from keras.models import Sequential
-from keras.layers import LSTM, Dropout, Dense
+from keras.layers import LSTM, Dropout, Dense, Flatten
 
 model = Sequential()
 
-model.add(LSTM(units=50,return_sequences=True, input_shape=(X_train.shape[1], 1)))
+model.add(LSTM(units=50,return_sequences=True, input_shape=(X_train.shape[1], 5)))
 model.add(Dropout(0.2))
 
 model.add(LSTM(units=50,return_sequences=True))
@@ -62,34 +62,41 @@ model.add(Dropout(0.2))
 
 model.add(Dense(units=1))
 
+model.summary()
 model.compile(optimizer='adam', loss= 'mean_squared_error')
 
 model.fit(X_train,y_train,epochs=100, batch_size=32)
 
 """Test the Results"""
 
+# Read in test dataset
 dataset_test = pd.read_csv('ZION5Min.csv', sep=r'\s*,\s*')
-real_stock_price = dataset_test.iloc[:,0:1].values
+real_stock_price = dataset_test.to_numpy()
 
-dataset_total = dataset_test['Open']
-dataset_total.shape
-inputs = dataset_total[:].values
-inputs = inputs.reshape(-1,1)
-inputs = sc.transform(inputs)
+# Reshape data
+inputs = sc.transform(dataset_test[:].values)
 X_test = []
 for i in range(10, 1000):
-  X_test.append(inputs[i-10:i, 0])
+  X_test.append(inputs[i-10:i])
 X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1],  1))
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1],  5))
 
-print(X_test.shape)
-
+# Predict next prices
 predicted_stock_price = model.predict(X_test)
-predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
+# Add 4 columns of 0 onto predictions so it can be fed back through sc
+shaped_predictions = np.empty(shape = (990, 5))
+for row in range(0, 990):
+  shaped_predictions[row, 0] = predicted_stock_price[row, 0]
+  for col in range (1, 5):
+    shaped_predictions[row, col] = 0
+# un-Normalize data
+predicted_stock_price = sc.inverse_transform(shaped_predictions)
+
+# Graph results
 import matplotlib.pyplot as plt
-plt.plot(real_stock_price, color = 'black', label = 'AMD Stock Price')
-plt.plot(predicted_stock_price, color = 'green', label = 'AMD Prediction')
+plt.plot(real_stock_price[:,0], color = 'black', label = 'AMD Stock Price')
+plt.plot(predicted_stock_price[:,0], color = 'green', label = 'AMD Prediction')
 plt.title('AMD Prediction')
 plt.xlabel('Time')
 plt.ylabel('AMD Stock Price')
